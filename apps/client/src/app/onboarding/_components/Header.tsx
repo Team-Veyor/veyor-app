8438fb3aceee6d434ebfb48af943e99023595d64
namdaeun <nde40345@gmail.com>
2026-06-09 17:45:34 +0900

    feat: add back router

--- apps/client/src/app/onboarding/_components/Header.tsx
+++ apps/client/src/app/onboarding/_components/Header.tsx
@@ -1,9 +1,18 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
 import ChevronLeftIcon from '@/assets/icons/ChevronLeftIcon';
 
 const Header = () => {
+  const router = useRouter();
+
   return (
     <header className='flex items-center justify-between h-[44px]'>
-      <button type='button' className='py-2.5 pl-[6px] pr-[14px]'>
+      <button
+        type='button'
+        className='py-2.5 pl-[6px] pr-[14px] cursor-pointer'
+        onClick={() => router.back()}
+      >
         <ChevronLeftIcon />
       </button>
     </header>
