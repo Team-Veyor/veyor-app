64c9b1068f6dfeadc5064cd0e5c5647327fef0db
namdaeun <nde40345@gmail.com>
2026-06-10 18:26:16 +0900

    feat: add today survey section

--- apps/client/src/app/home/page.tsx
+++ apps/client/src/app/home/page.tsx
@@ -1,14 +1,24 @@
+import TodaySurvey from '@/app/home/_components/TodaySurvey';
+import WeeklyStreakCard from '@/app/home/_components/WeeklyStreakCard';
 import CalendarIcon from '@/assets/icons/CalendarIcon';
 import Callout from '@/components/Callout/Callout';
 
 const HomePage = () => {
   return (
-    <div>
+    <div className='flex flex-col gap-12'>
       <Callout
         type='success'
-        icon={<CalendarIcon className='size-4' />}
+        icon={<CalendarIcon className='size-16' />}
         title='설문 게시 시간: 매일 오전 10시'
       />
+      <TodaySurvey
+        title='데일리 보부상 가방 디자인 설문조사'
+        rewardAmount={1000}
+        estMinutes={'2~3'}
+        url='https://www.google.com'
+        participated={false}
+      />
+      <WeeklyStreakCard streak={3} weeklyStatus={['mon', 'tue', 'wed']} totalRewardAmount={1000} />
     </div>
   );
 };
