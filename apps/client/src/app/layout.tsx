5eb21e96734f22ca8728255d58e662f5f19a7f99
namdaeun <nde40345@gmail.com>
2026-06-25 23:20:57 +0900

    fix(client): set maximumScale in viewport

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -36,6 +36,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
+  maximumScale: 1,
   viewportFit: 'cover',
   themeColor: '#09ba8e',
 };
