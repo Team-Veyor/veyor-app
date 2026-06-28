58b2563c9486f1b776cb8fef14e387ed4f02bc1c
mingyu-aicx <mingyu.shin@aicx.kr>
2026-06-28 10:00:54 +0900

    fix(client): refresh home survey periodically

--- apps/client/src/app/home/_hooks/useHomeQuery.ts
+++ apps/client/src/app/home/_hooks/useHomeQuery.ts
@@ -3,11 +3,13 @@ import { getHome } from '@/app/home/_apis/home';
 
 // TODO: 쿼리키 일괄 관리
 export const HOME_QUERY_KEY = ['home'] as const;
+const HOME_REFETCH_INTERVAL_MS = 30_000;
 
 const useHomeQuery = () =>
   useQuery({
     queryKey: HOME_QUERY_KEY,
     queryFn: getHome,
+    refetchInterval: HOME_REFETCH_INTERVAL_MS,
   });
 
 export default useHomeQuery;
